32a5dac5a91b97fffa10dc4c042e495b232c8890
Philip <philip.iparaguirre@gmail.com>
2020-10-02 06:55:46 -0700

    build

---
+++ build/precache-manifest.204337d7aa549088a346fa6351487c1c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3582465472b20b66cef1fc301e68ef8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78398900e59aa5b5ad05",
+    "url": "/static/css/main.e83077c1.chunk.css"
+  },
+  {
+    "revision": "9037a3cce25991c42fb2",
+    "url": "/static/js/2.94f45c91.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.94f45c91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78398900e59aa5b5ad05",
+    "url": "/static/js/main.4dfd2e92.chunk.js"
+  },
+  {
+    "revision": "e0fc635ae829bfb13ebc",
+    "url": "/static/js/runtime-main.dcf2d509.js"
+  },
+  {
+    "revision": "d363faaf8d24af0fd05f3e26a1f0c962",
+    "url": "/static/media/cech.d363faaf.jpg"
+  },
+  {
+    "revision": "003a25ecddb64e48a3a1a67b4e57b714",
+    "url": "/static/media/chillwell.003a25ec.jpg"
+  },
+  {
+    "revision": "58833cb5228b41e387153f57cef8cd1b",
+    "url": "/static/media/footer.58833cb5.jpeg"
+  },
+  {
+    "revision": "6ceca9762df555e0c6af06a697726f80",
+    "url": "/static/media/goal.6ceca976.jpg"
+  },
+  {
+    "revision": "20859d674342b83d0cade380836245a8",
+    "url": "/static/media/havertz.20859d67.jpg"
+  },
+  {
+    "revision": "04edc6ed5a358e228e200e446821c3a0",
+    "url": "/static/media/logo.04edc6ed.png"
+  },
+  {
+    "revision": "5c43376d00baba348c5d05fb456de368",
+    "url": "/static/media/mendy.5c43376d.jpg"
+  },
+  {
+    "revision": "8901c1b3fdc612a4b0f41318bce94761",
+    "url": "/static/media/pulisic.8901c1b3.jpg"
+  },
+  {
+    "revision": "0b773d4cddb0754153dd6a6e972ee0ec",
+    "url": "/static/media/reece.0b773d4c.jpeg"
+  },
+  {
+    "revision": "c6ec779129079983bee83483160acf13",
+    "url": "/static/media/womens.c6ec7791.jpg"
+  }
+]);
